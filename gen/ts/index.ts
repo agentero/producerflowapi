377f3a9c19e52a632be3dd3e30c9f23e85b3b129
jorge-ferrero-ag <jorge.ferrero@agentero.com>
2025-06-17 13:45:26 +0200

    chore: improve ts generation

--- gen/ts/index.ts
+++ gen/ts/index.ts
@@ -1,4 +1,5 @@
 // Generated ProducerFlow API types
 export * from "./producerflow/producer/v1/producer_pb";
+export * from "./producerflow/appointment/v1/appointment_pb";
 export * from "./google/type/dayofweek_pb";
 export * from "./google/type/timeofday_pb"; 
